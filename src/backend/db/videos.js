84d4fe03756fa7489c3df3db6828297ce019352b
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-12-26 23:57:00 +0530

    fix: updated database

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,58 +5,58 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: " Kasoor",
-    videoId: "BmUe3-sfr7E",
+    _id: "xOsZj48vXGA",
+    title: "Roz Roz",
+    videoId: "xOsZj48vXGA",
     description:
       "Haan, mai gumsum hoon In raahon ki tarah Tere khwabon mei Teri khwahishon mei chhupa",
-    creator: "Prateek Kuhad",
+    creator: "Rajan and Shipla",
     thumbnail:
-      "https://ik.imagekit.io/1a4v4n07o/tn1_Rwuuak97f?ik-sdk-version=javascript-1.4.3&updatedAt=1670165594600",
+      "https://ik.imagekit.io/1a4v4n07o/roz_rozwebp.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1671805878266",
     category: "Indie",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "QjsnYR_ty",
-    title: "Aazaad",
-    videoId: "U9xK-EwrWTk",
+    _id: "YEt23lSMR8o",
+    title: "Nasha",
+    videoId: "YEt23lSMR8o",
     description:
       "I'm sure you've been there or thereabouts in your own way, but there may come a time when you succumb to self-doubt and find it hard to crawl out of that place. What that place does is not let you be; it begins to feed off you and nibble at the qualities that set you apart. I have had that phase subconsciously, consistently linger on for an anxiety-inducing two or more years and while I kept dreaming about the same things that I had grown up wishing for, my motivation to do something about them was damaged, and this realisation would stare me in my face right when it truly mattered.",
     creator: "Samar Mehdi",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/azaad_P1MT0lcxb.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648641560838",
+      "https://ik.imagekit.io/1a4v4n07o/nashawebp.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1671805878390",
     category: "Indie",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "Wo5djdDj",
-    title: "TERE BINA",
-    videoId: "VQeGKNOFe2Y",
+    _id: "ZJ34geQLtUE",
+    title: "Aakhon se Batana",
+    videoId: "ZJ34geQLtUE",
     description:
       "'Tere Bina' is a song of love song for yourself. I wrote this song to remind myself that irrespective of what happens, who comes or leaves, I will always be there for myself! You learn the meaning of true love when you lose it, and 'Tere Bina' is a subtle reminder.",
-    creator: "Saby Singh",
+    creator: "Dikshant",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/saby-2_WhBBLWDQI.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648643315705",
+      "https://ik.imagekit.io/1a4v4n07o/aakhonse_batana_6s.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1671805905717",
     category: "Indie",
     isLiked: false,
     watchLater: false,
   },
   {
     _id: "WosdajEP_BbI",
-    title: "Bandey",
-    videoId: "F4qqL1aBu5E",
+    title: "Liggi",
+    videoId: "WosdajEP_BbI",
     description: "Bandey from The Local Train's debut album 'Aalas Ka Pedh'",
-    creator: "The Local Train",
+    creator: "Ritviz",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/bandey_PC4azA1c4_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648641962804",
+      "https://ik.imagekit.io/1a4v4n07o/liggi.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1671805905931",
     category: "Indie",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "WoShcjn_U",
+    _id: "ZRVxoAclJlI",
     title: "Ghost",
     videoId: "ZRVxoAclJlI",
     description:
@@ -69,111 +69,111 @@ export const videos = [
     watchLater: false,
   },
   {
-    _id: "WoSsxagt",
-    title: "Vaaqif Live(d)",
-    videoId: "RYg_PhYY2bA",
+    _id: "eHyl9KG9RWA",
+    title: "3:49 A.M",
+    videoId: "eHyl9KG9RWA",
     description:
       "Usulon ki jo thi duniya ab hai kahan bolo Baatein kitaabi jo suni humein na yakeen dekho Guftgu aise mazahabi par ab mujhko gumaan Hoon Vaaqif na hoon nadaan koi na fikrey jahaanKyun naamanzoor o huzur kirdaar hai yahan Iss kahani kaa na magarur bekasoor hoon gunjati hai jo dil ki jubaan",
-    creator: "The Local Train",
+    creator: "Divine",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/vaqif-local-train_QRQ9tZ1uF.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648858881167",
+      "https://ik.imagekit.io/1a4v4n07o/pop/divine.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672066251646",
     category: "Pop",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "WoTYcatyT",
-    title: "Aaftaab (Official Audio)",
-    videoId: "U77d9912lrw",
+    _id: "pxCWiYFkvTg",
+    title: "Aatma Rama",
+    videoId: "pxCWiYFkvTg",
     description:
       "The unconventional & simplistic title of this song doesn't do nearly enough justice to describe how truly out-of-the-ordinary this track from The Local Train's set is. 'Aaftaab' from The Local Train's 2nd album 'Vaaqif'",
-    creator: "The Local Train",
+    creator: "Brodha V",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/aaftab-thelocaltrain_6Qkukqu6I.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648859013097",
+      "https://ik.imagekit.io/1a4v4n07o/pop/aatma_rama.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672066251509",
     category: "Pop",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "WOcUXuck",
-    title: "Tu Kisi Rail Si",
+    _id: "bWwwWnNh8I0",
+    title: "Tandav Nach",
     videoId: "GQEoKDivm58",
     description:
       "I sometimes feel bad in how can we oversee such a masterpiece. Stop making commercial movies, Stop following glamour. Nothing matters if you can hit the right cords with your movie. Masaan is in the true sense a modern day masterpiece",
-    creator: "The Indian Ocean",
+    creator: "Dino James",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/masaan_lKyYX5Dzv.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648859226890",
+      "https://ik.imagekit.io/1a4v4n07o/pop/tandav.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672066251416",
     category: "Pop",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "dckjsxcXuck",
-    title: "H Y P E R - F O C U S⚡️",
+    _id: "Ss80v5Tqlss",
+    title: "Dil Janiya",
     videoId: "Gst3OsuZM1s",
     description:
       "I didn't think a Study With Me would make me more productive especially during quarantine, but your videos have really opened up a new perspective on studying. Plus it's cool to see another Canadian on youtube and giving me perspective in the medical field, since I am currently a high school student looking into pursuing medicine. Keep it up!",
-    creator: "StudyMD",
+    creator: "Sonu Nigam",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/smooth-jazz_Z1qV__Nei.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648859567275",
-    category: "Background",
+      "https://ik.imagekit.io/1a4v4n07o/Romance/dil_janiya_1_.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672066965098",
+    category: "Romance",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "dccsgyXuck",
-    title: "Interstellar",
+    _id: "64HMtTbBEx8",
+    title: "Chitta",
     videoId: "TKILNbrWxBQ",
     description:
       "Tushar Lall’s arrangement of Interstellar Music (originally composed by Hans Zimmer) on Indian Classical Instruments. This is his humble attempt to portray the power and versatility of Indian Classical instruments. This is a tribute to the original makers of this song and we thank them for their incredible contribution to music.All the videos, songs, images, and graphics used in the video belong to their respective owners and I or this channel does not claim any right over them.",
-    creator: "Tushar Lal",
+    creator: "Neha Basin",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/interstellat-tijp_NIxO0osCc.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648859566829",
-    category: "Background",
+      "https://ik.imagekit.io/1a4v4n07o/Romance/mqdefault_6s.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672066965278",
+    category: "Romance",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "suibqkFCHs",
-    title: "Titanic Music",
+    _id: "tDlLnutp0Q0",
+    title: "Koi Jane Na",
     videoId: "IzpviJVHxUs",
     description:
       "Tushar Lalls arrangement of Interstellar Music (originally composed by Hans Zimmer) on Indian Classical Instruments. This is his humble attempt to portray the power and versatility of Indian Classical instruments. This is a tribute to the original makers of this song and we thank them for their incredible contribution to music.All the videos, songs, images, and graphics used in the video belong to their respective owners and I or this channel does not claim any right over them.",
-    creator: "Tushar Lal",
+    creator: "Raghav Kaushik",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/tijp-titanic_Tl8B4qV42.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648859566888",
-    category: "Background",
+      "https://ik.imagekit.io/1a4v4n07o/Romance/koi_jane_na.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672066965509",
+    category: "Romance",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "pokoicwTs",
-    title: "Unnai Kaanadhu Naan",
+    _id: "0uKRRt3GogU",
+    title: "Shape Of You",
     videoId: "ySo9IDdRuck",
     description:
       "The Berklee Indian Ensemble performs Unnai Kaanadhu Naan by Shankar-Ehsaan-Loy in Studio A at the Power Station at BerkleeNYC. The song was arranged by the Berklee Indian Ensemble. Check out the 360 performance video here",
-    creator: "BerkleeNYC",
+    creator: "Mahesh Raghavan",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/berkelee-ins_yhfTXMQ1r.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648860006898",
+      "https://ik.imagekit.io/1a4v4n07o/Instrumental/shape_of_you.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672066393114",
     category: "Instrumental",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "TYVDSsher",
-    title: "Berklee",
+    _id: "5qBSlzRY6yY",
+    title: "Zara Zara",
     videoId: "dn_1O3J56E8",
     description:
       "It was indeed an honor for our Berklee Indian Ensemble musicians to reinterpret such an iconic composition, and collaborate on this live performance with Shankar Ji as part of the fourth Berklee India Exchange (BIX) Artist Residency. We also dedicate this video in tribute to John McLaughlin who will receive an Honorary Doctorate from Berklee College of Music on July 10th, 2017 in Valencia, Spain. ",
-    creator: "BerkleeNYC",
+    creator: "Rahul Krishnan",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/berklee-ins_TKaJUCmXP.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648860006853",
+      "https://ik.imagekit.io/1a4v4n07o/Instrumental/mqdefault_6s.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1672066392691",
     category: "Instrumental",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "TTYSxmms",
+    _id: "Do3v9yy_d3I",
     title: "Talal Qureshi",
     videoId: "Do3v9yy_d3I",
     description:
@@ -186,7 +186,7 @@ export const videos = [
     watchLater: false,
   },
   {
-    _id: "UTxYRsk",
+    _id: "vxUjwGDxR3w",
     title: "CHITTA' (Official Video)",
     videoId: "vxUjwGDxR3w",
     description:
@@ -199,7 +199,7 @@ export const videos = [
     watchLater: false,
   },
   {
-    _id: "UsazyyRYRsk",
+    _id: "OM7ow6VR7JY",
     title: "Amar (Video)",
     videoId: "OM7ow6VR7JY",
     description:
@@ -212,40 +212,40 @@ export const videos = [
     watchLater: false,
   },
   {
-    _id: "UBscyRsk",
-    title: "What About Me?",
+    _id: "GZYP1ZjY498",
+    title: "Channa Mereya",
     videoId: "fuhHU_BZXSk",
     description:
       "Recorded and filmed live (free of overdubs) from October 7-10 at Kytopia Studios in Utrecht, the Netherlands, for GroundUP Music. For more information and upcoming tour dates, please visit http://groundup.ropeadope.com or http://www.snarkypuppy.com.",
-    creator: "Snarky Puppy",
+    creator: "Arijit Singh",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/live-2_-ycDS5OrR.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648860946775",
+      "https://ik.imagekit.io/1a4v4n07o/live/chnna_mereya.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1671806705165",
     category: "Live",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "UBscGVyRsk",
-    title: "Lingus",
+    _id: "h7gyJRWrjbg",
+    title: "Tujhe Kitna",
     videoId: "L_XJ_s5IsQc",
     description:
       "The central nervous system of the octopus is structured so that each limb, quite literally, has a mind of its own. I must say, that octopus on the drums is doing an excellent human impression.",
-    creator: "Snarky Puppy",
+    creator: "Jubin Nautiyal",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/live-1_jIpTfb_G6.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648860946825",
+      "https://ik.imagekit.io/1a4v4n07o/live/kabir_singh.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1671806704706",
     category: "Live",
     isLiked: false,
     watchLater: false,
   },
   {
-    _id: "UBsUTjsc",
-    title: "Hideaway",
+    _id: "CVLpV3K0hyY",
+    title: "Baarishein",
     videoId: "sY8pCnKSSVg",
     description:
       "An orchestral performance of 'Hideaway' at the North Sea Jazz Festival 2017, performed on July 9th by Jacob & the Metropole Orkest. Arranged and orchestrated by Jacob Collier; additional orchestration by Jamshied Sharifi.",
-    creator: "Jacob Collier",
+    creator: "Anuv Jain",
     thumbnail:
-      "https://ik.imagekit.io/avavya/VideoLib/live-3_54LEUbeR3.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1648860947014",
+      "https://ik.imagekit.io/1a4v4n07o/live/anuv_jain.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1671806699237",
     category: "Live",
     isLiked: false,
     watchLater: false,
